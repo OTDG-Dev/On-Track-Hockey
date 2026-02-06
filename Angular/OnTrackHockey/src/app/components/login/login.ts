4cc95420b7e7485ca1b4052e3a9720519bb2c88c
Anthony Siragusa <ajsirag@gmail.com>
2026-02-05 19:02:29 -0600

    added register page + basic routing + small tweaks to login page

--- Angular/OnTrackHockey/src/app/components/login/login.ts
+++ Angular/OnTrackHockey/src/app/components/login/login.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [FormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  username: string = "";
+  password: string = "";
+
+  onClick() {
+    console.log(this.username + " : " + this.password);
+  }
 
 }
