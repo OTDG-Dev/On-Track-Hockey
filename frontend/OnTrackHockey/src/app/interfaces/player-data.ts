ce59f89fa621dc3f0e624836431d019c008ce6ec
Anthony Siragusa <ajsirag@gmail.com>
2026-02-11 19:31:50 -0600

    removed subfolder

--- frontend/OnTrackHockey/src/app/interfaces/player-data.ts
+++
@@ -1,9 +0,0 @@
-export interface PlayerData {
-    first_name: string
-    last_name: string,
-    sweater_number: number,
-    position: string,
-    birth_date: string,
-    birth_country: string,
-    shoots_catches: string
-}
