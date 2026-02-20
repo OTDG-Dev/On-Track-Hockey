ad9925921cfbac1b6868b38afe3b242b589b7cc4
Anthony Siragusa <ajsirag@gmail.com>
2026-02-20 00:52:43 -0600

    added html and initial styling

--- frontend/src/app/components/view-player/view-player.ts
+++ frontend/src/app/components/view-player/view-player.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal, WritableSignal } from '@angular/core';
 import { PlayerService } from '../../services/player-service';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 
@@ -9,6 +9,17 @@ import { ActivatedRoute, RouterModule } from '@angular/router';
   styleUrl: './view-player.css',
 })
 export class ViewPlayer {
+
+  first_name: WritableSignal<string> = signal("");
+  last_name: WritableSignal<string> = signal("");
+  sweater_number: WritableSignal<number> = signal(-1);
+  position: WritableSignal<string> = signal("");
+  birth_date: WritableSignal<string> = signal("");
+  birth_country: WritableSignal<string> = signal("");
+  shoots_catches: WritableSignal<string> = signal("");
+  team_full_name: WritableSignal<string> = signal("");
+  team_short_name: WritableSignal<string> = signal("");
+  avatarUrl: WritableSignal<string> = signal("https://a.espncdn.com/combiner/i?img=/i/headshots/nhl/players/full/5149125.png&w=350&h=254");
 
   constructor(private playerService: PlayerService, private route: ActivatedRoute) {}
 
@@ -24,7 +35,15 @@ export class ViewPlayer {
     .subscribe(
       {
         next: (responseData) => {
-          console.log(responseData.player);
+          this.first_name.set(responseData.player.first_name);
+          this.last_name.set(responseData.player.last_name);
+          this.sweater_number.set(responseData.player.sweater_number);
+          this.position.set(responseData.player.position);
+          this.birth_date.set(responseData.player.birth_date);
+          this.birth_country.set(responseData.player.birth_country);
+          this.shoots_catches.set(responseData.player.shoots_catches);
+          this.team_full_name.set(responseData.player.team_full_name);
+          this.team_short_name.set(responseData.player.team_short_name);
         },
         error: (err) => {
           console.log(err);
