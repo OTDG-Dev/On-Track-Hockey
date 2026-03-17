c2bda73ba4d09a068db792bb9e3cce1c37e4abf6
Matt <iambrendamore@gmail.com>
2026-03-17 18:47:15 -0400

    add: endpoint to get all teams in a div (#87)

--- backend/cmd/api/divisions.go
+++ backend/cmd/api/divisions.go
@@ -169,3 +169,26 @@ func (app *application) deleteDivisionHandler(w http.ResponseWriter, r *http.Req
 		app.serverErrorResponse(w, r, err)
 	}
 }
+
+func (app *application) listDivisionTeamHandler(w http.ResponseWriter, r *http.Request) {
+	id, err := app.readIDParam(r)
+	if err != nil {
+		app.badRequestResponse(w, r, err)
+		return
+	}
+
+	teams, err := app.models.Division.GetAllTeams(id)
+	if err != nil {
+		switch {
+		case errors.Is(err, data.ErrRecordNotFound):
+			app.notFoundResponse(w, r)
+		default:
+			app.serverErrorResponse(w, r, err)
+		}
+		return
+	}
+	err = app.writeJSON(w, http.StatusOK, envelope{"teams": teams}, nil)
+	if err != nil {
+		app.serverErrorResponse(w, r, err)
+	}
+}
